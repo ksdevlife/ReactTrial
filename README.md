32f6dbcc68f3fbfbe16df3605e2e9b0c1e01a00d
ksdevlife <developLifeWorker@gmail.com>
2018-06-27 13:15:56 +0900

    changing to be able to build react

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ReactTrial
+$ npm install
+$ node node_modules/webpack/bin/webpack.js
+$ npm start
+access with webbrowser
--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-"use strict"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//const App = () => ( 
+//<div> Hello React! </div>
+//);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello React!</div>
+    );
+  }
+}
+
+//const render = () => ReactDOM.render( <App /> , document.getElementById('root'));
+//render();
+ReactDOM.render(<App />, document.getElementById('root'));
